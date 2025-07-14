4a2632a719a20407c16a91130e55319eb622e587
João <contato@joaofelipegomes.com>
2025-07-14 09:40:45 -0300

    file upload

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,19 +1,53 @@
-import { ArrowLeft, Radio } from "lucide-react";
+import { ArrowLeft, LoaderCircle, Radio, Upload } from "lucide-react";
+import { useRef, useState } from "react";
 import { Link, Navigate, useParams } from "react-router-dom";
 import { QuestionForm } from "@/components/question-form";
 import { QuestionList } from "@/components/question-list";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 type RoomParams = {
   roomId: string;
 };
 
 export function Room() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [uploading, setUploading] = useState(false);
   const params = useParams<RoomParams>();
 
   if (!params.roomId) {
     return <Navigate replace to="/" />;
   }
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  interface FileChangeEvent extends React.ChangeEvent<HTMLInputElement> {}
+
+  const handleFileChange = async (e: FileChangeEvent): Promise<void> => {
+    const audioFile: File | undefined = e.target.files?.[0];
+    if (!audioFile) {
+      return;
+    }
+
+    setUploading(true);
+
+    const formData = new FormData();
+    formData.append("file", audioFile, "audio.webm");
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    await response.json();
+    setUploading(false);
+  };
 
   return (
     <div className="bg-zinc-950 min-h-screen">
@@ -26,12 +60,30 @@ export function Room() {
                 Voltar ao Início
               </Button>
             </Link>
-            <Link to={`/room/${params.roomId}/audio`}>
-              <Button className="flex items-center gap-2" variant="secondary">
-                <Radio className="size-4" />
-                Gravar Áudio
+            <div className="flex flex-row items-center gap-3">
+              <Input
+                className="hidden"
+                onChange={handleFileChange}
+                ref={fileInputRef}
+                type="file"
+              />
+              <Button
+                className="flex items-center gap-2"
+                disabled={uploading}
+                onClick={handleButtonClick}
+                variant="secondary"
+              >
+                <Upload className="size-4" />
+                Subir Áudio
+                {uploading && <LoaderCircle className="animate-spin" />}
               </Button>
-            </Link>
+              <Link to={`/room/${params.roomId}/audio`}>
+                <Button className="flex items-center gap-2" variant="secondary">
+                  <Radio className="size-4" />
+                  Gravar Áudio
+                </Button>
+              </Link>
+            </div>
           </div>
           <h1 className="mb-2 font-bold text-foreground text-3xl">
             Sala de Perguntas
